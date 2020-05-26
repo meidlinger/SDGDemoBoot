8be9bf1952b87c8cedfb1ad8f90a7edea34a6117
Kenny Yang <kenny.j.yang@gmail.com>
2020-05-26 18:57:15 -0500

    update db driver

--- src/main/java/sdg/demo/java/MysqlCon.java
+++ src/main/java/sdg/demo/java/MysqlCon.java
@@ -7,7 +7,7 @@ class MysqlCon {
 	private PreparedStatement preparedStatement = null;
 	public MysqlCon() {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "root", "root");
 		} catch (Exception e) {
 			System.out.println(e);
