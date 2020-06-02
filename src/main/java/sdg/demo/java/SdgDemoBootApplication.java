1b380454764d7f6053b780d32f936cb37dd1d5b5
Kenny Yang <kenny.j.yang@gmail.com>
2020-06-01 20:41:46 -0500

    update

--- src/main/java/sdg/demo/java/SdgDemoBootApplication.java
+++ src/main/java/sdg/demo/java/SdgDemoBootApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import io.jaegertracing.Configuration;
 @SpringBootApplication
 public class SdgDemoBootApplication {
 
@@ -12,8 +11,10 @@ public class SdgDemoBootApplication {
 		SpringApplication.run(SdgDemoBootApplication.class, args);
 	}
 
+	/*
 	@Bean
 	public io.opentracing.Tracer tracer() {
 		return Configuration.fromEnv().getTracer();
 	}
+	*/
 }
